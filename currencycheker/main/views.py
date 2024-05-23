5c804ae162b3e4a67afd4d56287421530eaa293c
Strange Milan <strangem@studen.21-school.ru>
2024-05-23 21:52:25 +0300

    project done

--- currencycheker/main/views.py
+++ currencycheker/main/views.py
@@ -1,8 +1,6 @@
-import json
 from dateutil import parser as date_parser
 
 from django.contrib import messages
-from django.core.serializers.json import DjangoJSONEncoder
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 
@@ -30,10 +28,14 @@ def chart_view(request):
 
 def chart_data(request):
     selected_countries = request.session.get('selected_countries', [])
+    start_date = date_parser.parse(request.session.get('start_date'))
+    end_date = date_parser.parse(request.session.get('end_date'))
     countries = Country.objects.filter(name__in=selected_countries)
     data = {}
     for country in countries:
-        currency = Currency.objects.filter(code=country.currency_code).order_by("date")
+        parser = CurrencyRateParser(country.currency_code)
+        parser.fetch_currency_rates(start_date, end_date)
+        currency = Currency.objects.filter(code=country.currency_code, date__range=(start_date, end_date)).order_by("date")
         data[country.name] = list(currency.values('date', 'rate'))
 
     return JsonResponse(data)
@@ -51,11 +53,14 @@ def fetch_currency_rates(request):
                 messages.error(request,
                                'Диапазон дат не должен превышать двух лет и дата начала периода должна быть раньше даты конца.')
                 return redirect('index')
+            request.session['start_date'] = str(start_date)
+            request.session['end_date'] = str(end_date)
         except ValueError:
             messages.error(request, 'Неверно введена дата.')
             return redirect('index')
 
         CountryParser.parse_country()
+
         return render(request, 'main/country_list.html',
                       {"countries": Country.objects.all().order_by("name")})
     return redirect('index')
